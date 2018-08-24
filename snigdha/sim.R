866036ffac3d43ddd26a64ea002fc8c43bdb67a5
Jonathan Taylor <jonathan.taylor@stanford.edu>
2018-08-23 20:37:45 -0700

    ryan sim.R file

--- snigdha/sim.R
+++ snigdha/sim.R
@@ -577,3 +577,25 @@ print.tex = function(tab, tab.se=NULL, digits=3, file=NULL, align="l") {
   }
   cat(paste0("\\end{tabular}\n"), file=file, append=T)
 }
+
+# Enlist function
+
+enlist <- function (...) 
+{
+    result <- list(...)
+    if ((nargs() == 1) & is.character(n <- result[[1]])) {
+        result <- as.list(seq(n))
+        names(result) <- n
+        for (i in n) result[[i]] <- get(i)
+    }
+    else {
+        n <- sys.call()
+        n <- as.character(n)[-1]
+        if (!is.null(n2 <- names(result))) {
+            which <- n2 != ""
+            n[which] <- n2[which]
+        }
+        names(result) <- n
+    }
+    result
+}
